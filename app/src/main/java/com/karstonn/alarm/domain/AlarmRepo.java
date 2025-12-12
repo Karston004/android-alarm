2dbf286ed14b57c5ac4544a6516236f410deac5b
Joe Smith <004jsmith@gmail.com>
2025-12-12 07:05:10 +0000

    Feat: Change named from AlarmRepoController to AlarmRepo

--- app/src/main/java/com/karstonn/alarm/domain/AlarmRepo.java
+++ app/src/main/java/com/karstonn/alarm/domain/AlarmRepo.java
@@ -2,7 +2,7 @@ package com.karstonn.alarm.domain;
 
 import java.util.List;
 
-public interface AlarmController {
+public interface AlarmRepo {
     public void setAlarm(Alarm alarm);
     public List<Alarm> listAlarms();
     public void removeAlarm(Alarm alarm);
