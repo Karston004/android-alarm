087086e2851511a198fb2e821782d33527c0ca64
Joe Smith <004jsmith@gmail.com>
2025-12-12 07:14:56 +0000

    Feat: Added ObserveableAlarmRepo

---
+++ app/src/main/java/com/karstonn/alarm/domain/ObserveableAlarmRepo.java
@@ -0,0 +1,6 @@
+package com.karstonn.alarm.domain;
+
+public interface ObserveableAlarmRepo extends  AlarmRepo{
+    public void addListener (AlarmRepoListener listener);
+    public void removeListener(AlarmRepoListener listener);
+}
