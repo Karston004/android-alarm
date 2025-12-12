e37d27df5f52140328e86f3eea291ab01d6031b8
Joe Smith <004jsmith@gmail.com>
2025-12-12 07:30:19 +0000

    Feat: Added a binder for Repo and Scheduler

---
+++ app/src/main/java/com/karstonn/alarm/domain/RepoSchedulerBinder.java
@@ -0,0 +1,10 @@
+package com.karstonn.alarm.domain;
+
+public final class RepoSchedulerBinder implements AlarmRepoListener {
+    AlarmScheduler alarmScheduler;
+    public RepoSchedulerBinder (AlarmScheduler scheduler) {this.alarmScheduler = scheduler;}
+    @Override
+    public void onAlarmAdded(Alarm alarm) {alarmScheduler.schedule(alarm);}
+    @Override
+    public void onAlarmRemoved(Alarm alarm) {alarmScheduler.cancel(alarm);}
+}
