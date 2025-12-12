2c1cb577fac5b6cec0f9c0fa407f71cc85e4f3c6
Joe Smith <004jsmith@gmail.com>
2025-12-12 04:54:08 +0000

    Feat: Prevent external mutation of AlarmManager alarms

---
+++ app/src/main/java/com/example/alarm/AlarmManager.java
@@ -0,0 +1,18 @@
+package com.example.alarm;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+public class AlarmManager implements  AlarmController{
+
+    List<Alarm> alarms = new ArrayList<>();
+    public void setAlarm(Alarm alarm){
+        alarms.add(new Alarm(alarm));
+    };
+    public List<Alarm> listAlarms(){
+        return  Collections.unmodifiableList(alarms);
+    };
+    public void removeAlarm(Alarm alarm){
+        alarms.remove(alarm);
+    };
+}
